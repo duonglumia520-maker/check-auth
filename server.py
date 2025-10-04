9ebc6591e7ea5b24686f73f7a75bd0cf40e7a1c2
duonglumia520-maker <duonglumia520@gmail.com>
2025-10-04 15:44:10 +0700

    Update server.py

--- server.py
+++ server.py
@@ -325,7 +325,7 @@ def get_log():
 # Cho phép Render chạy đúng cổng
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)        }
+    app.run(host="0.0.0.0", port=port)        
         with open(USED_PATH, "w") as f:
             json.dump(USED_CODES, f, indent=2)
 
